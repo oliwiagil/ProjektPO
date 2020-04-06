d0a5bd6861456592a7968e46991ad28634ee3379
Adryaen <aderter116@gmail.com>
2020-04-07 01:40:06 +0200

    Basics of project

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # This is a new branch for temporary stuff 
 # we can test code here and delete this branch in any time
+# 
+# This is maven project. Created using IntelliJ Idea.
+# additional file javafx-sdk-14 is a library
+# 	- to add a library we must go File/Project Structure.../Libraries
+# 	  and there hover over the top left corner where is a little plus, 
+#	  then choose Java, and then new window should be displayed 
+# 	  now give a path to file javafx-sdk-14/lib and apply changes
+#	- in File/Project Structure.../Project, I set Project SDK: 14(java version"14") 
+#	  and Project language level: X - Experimantal features
+#
+#
+# Author of the code in Main class is Oliwia
+# Sorry for bad english ;)
+
---
+++ src/main/java/Main.java
@@ -0,0 +1,148 @@
+//package sample;
+
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.util.ArrayList;
+
+public class Main extends Application {
+    public int WIDTH=400;
+    public int HEIGHT=600;
+    private int SPEED=30;
+    public Color TLO =Color.WHITE;
+
+    public Pane gamePane;
+    public Timeline timeline;
+    EventHandler<KeyEvent> eventHandler;
+    Ship gracz;
+    ArrayList<Bullet> bullets=new ArrayList<>();
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        gamePane =new Pane();
+        gracz=new Ship(Color.GREEN);
+        gamePane.getChildren().add(gracz);
+
+        Scene scene=new Scene(gamePane,WIDTH,HEIGHT,TLO);
+
+        primaryStage.addEventHandler(KeyEvent.KEY_PRESSED,stworzEH());
+        primaryStage.setTitle("SpaceInv");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        uplywczasu();
+    }
+
+    public EventHandler<KeyEvent> stworzEH(){
+        eventHandler = new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent wcisnieto) {
+                if(wcisnieto.getCode()==KeyCode.ESCAPE){System.exit(0);}
+                if(wcisnieto.getCode() == KeyCode.SPACE){gracz.shot();}
+                else if (wcisnieto.getCode() == KeyCode.LEFT || wcisnieto.getCode() == KeyCode.A) {
+                    gracz.moveA();
+                } else if (wcisnieto.getCode() == KeyCode.RIGHT || wcisnieto.getCode() == KeyCode.D) {
+                    gracz.moveD();
+                }
+            }
+        };
+        return eventHandler;
+    }
+
+    void uplywczasu(){
+        timeline = new Timeline(
+                new KeyFrame(
+                        Duration.millis(SPEED),
+                        event -> {
+                            ruch();
+                        }
+                )
+        );
+        timeline.setCycleCount(Animation.INDEFINITE);
+        //uruchamia uplyw czasu
+        timeline.play();
+    }
+
+    void ruch(){
+        //przemieszcza pociski statku
+        for(Bullet i : bullets){i.moveUp();}
+    }
+
+    public class Ship extends Rectangle{
+        //obecna pozycja statku
+        double currentX;
+        //jednostaka o jaka sie przemieszcza
+        double move=5;
+
+        Ship(Color color){
+            super(30,20,color);
+            //ustala poczatkowa pozycje
+            setTranslateX((double) WIDTH/2);
+            setTranslateY(HEIGHT-20);
+            currentX=(double) WIDTH/2;
+        }
+
+        void moveD() {
+            //30 to wartosc szerokosci prostokata podana w konstruktorze super
+            if (currentX+ move+30<=WIDTH) {
+                currentX += move;
+                setTranslateX(currentX);
+            }
+        }
+        void moveA(){
+            if (currentX-move>=0) {
+                currentX -= move;
+                setTranslateX(currentX);
+            }
+        }
+        void shot(){
+            bullets.add(new Bullet(currentX));
+            System.out.println("shot");
+        }
+    }
+
+    public class Bullet extends Rectangle{
+        //obecna wysokosc pocisku
+        double currentY;
+        //jednostaka o jaka sie przemieszcza
+        double move=5;
+
+        Bullet(double x){
+            super(10,20,Color.GREY);
+            //poczatkowa pozycja x jest taka jak statku ktory wystrzeliwuje
+            setTranslateX(x);
+            //poczatkowa wysokosc pocisku
+            setTranslateY(HEIGHT-10);
+            currentY=0.9*HEIGHT;
+            gamePane.getChildren().add(this);
+        }
+        //jak statek strzela do gory
+        void moveUp(){
+            if(currentY-move+20>=0) {
+                currentY -= move;
+                setTranslateY(currentY);
+            }
+            //  else{bullets.remove(this);}
+        }
+        //jak przeciwnicy strzelaja w dol
+        void moveDown(){
+            currentY+=move;
+            setTranslateY(currentY);
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
